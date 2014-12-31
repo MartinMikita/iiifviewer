bd5eb113fac8995f4b3586a9629d20209706798b
MartinMikita <martin@mikita.eu>
2014-12-31 21:49:54 +0100

    Added WebGL version of QtGDAL viewer (using OLv3 and IiifViewer plugin)

--- Makefile
+++ Makefile
@@ -5,20 +5,24 @@ PLOVR = plovr-81ed862.jar
 all: serve
 
 $(PLOVR):
-	curl https://plovr.googlecode.com/files/plovr-81ed862.jar > $(PLOVR)
+	curl -S -L https://plovr.googlecode.com/files/plovr-81ed862.jar > $(PLOVR)
 
 serve: $(PLOVR)
 	java -jar $(PLOVR) serve standalone-debug.json plugin-debug.json
 
 build: $(PLOVR) build/iiifviewer.js qtgdal
 
-build/iiifviewer.js: src/iiifsource.js src/iiifviewer.js
+build/iiifviewer.js: src/iiifsource.js src/iiifviewer.js src/exports.js src/standalone.js pluign.json standalone.json
 	@mkdir -p build
 	java -jar $(PLOVR) build standalone.json > build/iiifviewer.js
 	java -jar $(PLOVR) build plugin.json > build/ol-iiifviewer.js
 
-qtgdal: src/examples/qtgdal.php build/iiifviewer.js
+build/ol3.js:
+	curl -L  http://openlayers.org/en/v3.0.0/build/ol.js > build/ol3.js
+
+qtgdal: src/examples/qtgdal.php build/ol-iiifviewer.js build/ol3.js
 	php src/examples/qtgdal.php > build/qtgdal.html
+	php src/examples/qtgdal-webgl.php > build/qtgdal-webgl.html
 
 lint:
 	fixjsstyle --strict -r ./src
---
+++ src/examples/qtgdal-webgl.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>OpenLayers 3 QtGDAL Tiles</title>
+<meta http-equiv="imagetoolbar" content="no"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<style type="text/css">
+html, body { margin:0; padding: 0; height: 100%; width: 100%; }
+body { width:100%; height:100%; background: #ffffff; }
+#map { position: absolute; height: 80%; width: 100%; background-color: #FFFFFF; border: 1px black solid;}
+.olImageLoadError {  display: none !important; }
+label { text-align: center; display: block; max-width: 100%; margin: 6px 0 4px 1px; font-weight: bold; font-size: 12.6px; color: #393939; }
+input, select, button { vertical-align: top; }
+input[type="range"] { display: block; width: 100%; }
+.range { float: left; width: 150px; }
+#table { border-spacing: 20px; }
+table tr td { padding-left: 40px; padding-right: 40px; }
+</style>
+<script type="text/javascript">
+<?php
+include('build/ol3.js');
+include('build/ol-iiifviewer.js');
+?>
+
+  function loadGdalFile(size, zoom, fname)
+  {
+    var zmin = zoom[0];
+    var zmax = zoom[1];
+    var factors = [];
+    for(var i = zmin; i <= zmax; i++)
+      factors.push( 1 << i );
+    var data = {
+      "@id" : "gdal://" + fname.replace(/[ .]/g, "-"),
+      "width" : size[0],
+      "height" : size[1],
+      "scale_factors" : factors,
+      "tile_width" : 256,
+      "tile_height" : 256,
+      "formats" : [ "png" ],
+      "qualities" : [ "native" ],
+    };
+    var init = function(viewer) {
+      var map = viewer.getMap();
+      var layer = map.getLayers().item(0);
+
+      var brightness = document.getElementById('brightness');
+      brightness.oninput = function(e) {
+        layer.setBrightness(parseFloat(brightness.value));
+      };
+
+      var contrast = document.getElementById('contrast');
+      contrast.oninput = function(e) {
+        layer.setContrast(parseFloat(contrast.value));
+      };
+
+      var hue = document.getElementById('hue');
+      hue.oninput = function(e) {
+        layer.setHue(parseFloat(hue.value));
+      };
+
+      var saturation = document.getElementById('saturation');
+      saturation.oninput = function(e) {
+        layer.setSaturation(parseFloat(saturation.value));
+      };
+    };
+
+    if (ol.has.WEBGL) {
+      document.getElementById('no-webgl').style.display = 'none';
+      new IiifViewer('map', data, init, true);
+    }
+  }
+
+</script>
+</head>
+<body>
+<div id="no-webgl" style="background:red;width:600px;">
+      This example requires a browser that supports <a href="http://get.webgl.org/">WebGL</a>.
+</div>
+<form>
+  <table width="100%">
+  <tr>
+    <td><label>Brightness:</label><input id="brightness" type="range" min="-1" max="1" step="0.1" value="0" /></td>
+    <td><label>Contrast:</label><input id="contrast" type="range" min="0" max="5" step="0.1" value="1" /></td>
+    <td><label>Hue:</label><input id="hue" type="range" min="-3.14" max="3.14" step="0.1" value="0" /></td>
+    <td><label>Saturation:</label><input id="saturation" type="range" min="0" max="5" step="0.1" value="1" /></td>
+  </tr>
+  </table>
+</form>
+<div id="map">
+</div>
+</body>
+</html>
